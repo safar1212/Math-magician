e705e03602710fcad65a79bb344b5b668b940949
safar ali <miqra019@gmail.com>
2022-06-28 18:09:12 +0500

    remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+// import calculator.js from './components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit
           {' '}
